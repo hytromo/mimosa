d880eede84eb16ab9a5be2962e08f815d273bad3
Alexandros Solanos <solanosalex@gmail.com>
2025-07-27 20:34:01 +0200

    fileutil test

--- internal/utils/fileutil/fileutil.go
+++ internal/utils/fileutil/fileutil.go
@@ -7,7 +7,11 @@ import (
 
 // SaveJSON saves a struct as pretty-formatted JSON data to a specific path
 func SaveJSON(path string, dataToWrite interface{}) error {
-	bytesToWrite, _ := json.MarshalIndent(dataToWrite, "", "\t")
+	bytesToWrite, err := json.MarshalIndent(dataToWrite, "", "\t")
+
+	if err != nil {
+		return err
+	}
 
 	return os.WriteFile(path, bytesToWrite, 0644)
 }
