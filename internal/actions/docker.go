b940c57d0dec800477b1fe00ae1b8c4bed003b0b
Alexandros Solanos <solanosalex@gmail.com>
2025-08-24 11:27:13 +0200

    Now green

--- internal/actions/docker.go
+++ internal/actions/docker.go
@@ -2,11 +2,12 @@ package actions
 
 import (
 	"github.com/hytromo/mimosa/internal/cacher"
+	"github.com/hytromo/mimosa/internal/configuration"
 	"github.com/hytromo/mimosa/internal/docker"
 )
 
 // Retag reads the latest available tags in the cache entry and uses them to push the new tags in the command
-func (a *Actioner) Retag(cacheEntry cacher.Cache, parsedCommand ParsedCommand, dryRun bool) error {
+func (a *Actioner) Retag(cacheEntry cacher.Cache, parsedCommand configuration.ParsedCommand, dryRun bool) error {
 	latestTagByTargetCached, err := cacheEntry.GetLatestTagByTarget()
 
 	if err != nil {
