2b7dab985a58f187b567db55ff7171a8f4ed2471
Alexandros Solanos <solanosalex@gmail.com>
2025-10-26 18:13:26 +0100

    Parse debug earlier than execution

--- cmd/root.go
+++ cmd/root.go
@@ -11,13 +11,14 @@ var rootCmd = &cobra.Command{
 	Use:   "mimosa",
 	Short: "Zero-config docker image promotion",
 	Long:  `Mimosa creates a unique hash<->tag association for each docker build - for the same build, mimosa will retag your image, instead of building it again!`,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		forceDebug, _ := cmd.Flags().GetBool(debugFlag)
+		logger.InitLogging(nil, forceDebug)
+	},
 }
 
 func Execute() {
 	err := rootCmd.Execute()
-	forceDebug, _ := rootCmd.PersistentFlags().GetBool(debugFlag)
-
-	logger.InitLogging(nil, forceDebug)
 	if err != nil {
 		os.Exit(1)
 	}
