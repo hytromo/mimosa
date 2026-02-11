82dcf7d244236c5755ba831eb097bdb1fb706726
Alexandros Solanos <solanosalex@gmail.com>
2026-02-11 23:19:11 +0100

    Retagging simplification (#20)

--- internal/docker/remote.go
+++ internal/docker/remote.go
@@ -6,16 +6,11 @@ import (
 	"log/slog"
 
 	"github.com/google/go-containerregistry/pkg/name"
-	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 )
 
 func Get(ref name.Reference, options ...remote.Option) (*remote.Descriptor, error) {
 	return remote.Get(ref, remote.WithAuthFromKeychain(Keychain))
-}
-
-func WriteIndex(ref name.Reference, ii v1.ImageIndex, options ...remote.Option) (rerr error) {
-	return remote.WriteIndex(ref, ii, remote.WithAuthFromKeychain(Keychain))
 }
 
 // TagExists checks if a tag exists in the remote registry
